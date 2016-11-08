79e89103a9dd391768c5c136f88087aeba06e3f2
JiaxuanYou <229331845@qq.com>
2016-11-08 08:44:53 +0800

    run L1+L2 model

--- model_batch/train_for_hist_alldata_loop.py
+++ model_batch/train_for_hist_alldata_loop.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     index_all = index_all[list_keep,:]
 
 
-    for loop in range(1,2):
+    for loop in range(0,1):
         for predict_year in range(2009,2016):
             logging.basicConfig(filename=config.save_path+'log/train_for_hist_alldata_loop'+str(predict_year)+str(loop)+'.log',level=logging.DEBUG)
             # # split into train and validate
